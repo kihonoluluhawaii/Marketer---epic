600323f6672463986c3da62a4a916f6eda32383a
kiho <kihonoluluhawaii@gmail.com>
2024-09-25 00:00:21 +0900

    "fix: 리뷰반영"

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,12 +1,6 @@
 import pencilIcon from '../../../public/images/pencil.png';
-import {
-    MainLogo,
-    MenuItem,
-    NavMenu,
-    NavWrapper,
-    SeoButton,
-} from './NavStyles.tsx';
 import { useState } from 'react';
+import styled from '@emotion/styled';
 
 const Nav = () => {
     const [activeItem, setActiveItem] = useState('Home');
@@ -43,3 +37,56 @@ const Nav = () => {
 };
 
 export default Nav;
+
+const NavWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    max-width: 1140px;
+    height: 88px;
+`;
+
+const MainLogo = styled.img`
+    width: 172px;
+`;
+
+const NavMenu = styled.ul`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    flex-grow: 1;
+    list-style: none;
+`;
+
+const MenuItem = styled.li<{ isActive: boolean }>`
+    padding: 0 15px;
+    cursor: pointer;
+
+    span {
+        color: ${(props) => (props.isActive ? 'rgb(52, 152, 219)' : 'inherit')};
+        font-size: 16px;
+        font-weight: 500;
+    }
+
+    &:hover {
+        color: rgb(52, 152, 219);
+    }
+`;
+
+const SeoButton = styled.button`
+    padding: 17px 20px;
+    font-size: 14px;
+    font-weight: 600;
+    color: rgb(52, 152, 219);
+    border: 2px solid rgb(52, 152, 219);
+    border-radius: 5px;
+    cursor: pointer;
+    background: none;
+    white-space: nowrap;
+
+    &:hover {
+        color: white;
+        background-color: rgb(52, 152, 219);
+        transition: 0.4s;
+    }
+`;
