15c171ebb242a8c0e8f92307a1d8119e1852e951
kiho <kihonoluluhawaii@gmail.com>
2024-09-25 16:01:46 +0900

    "fix: 리뷰반영 Icon 컴포넌트 생성"

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,36 +1,74 @@
-import xIcon from '/images/twitter.svg';
-import phoneIcon from '/images/phone.svg';
-import locationIcon from '/images/location.svg';
 import styled from '@emotion/styled';
+import Icon from '../Icon/Icon.tsx';
 
 const Header = () => {
     return (
         <Container>
             <HeaderTopContainer>
                 <HeaderTopLeft>
-                    <IconWrapper hoverColor="dodgerblue">
-                        <IconStyled src={xIcon} alt="x logo image" />
-                    </IconWrapper>
-                    <IconWrapper hoverColor="red">
-                        <IconStyled src={xIcon} alt="x logo image" />
-                    </IconWrapper>
-                    <IconWrapper hoverColor="blue">
-                        <IconStyled src={xIcon} alt="x logo image" />
-                    </IconWrapper>
-                    <IconWrapper hoverColor="lime">
-                        <IconStyled src={xIcon} alt="x logo image" />
-                    </IconWrapper>
-                    <IconWrapper hoverColor="skyblue">
-                        <IconStyled src={xIcon} alt="x logo image" />
-                    </IconWrapper>
+                    <SnsIconWrapper>
+                        <Icon
+                            name="x"
+                            wrapperSize={42}
+                            size={17}
+                            color={'rgb(228, 241, 254)'}
+                            activeColor={'skyblue'}
+                        />
+                    </SnsIconWrapper>
+                    <SnsIconWrapper>
+                        <Icon
+                            name="pinterest"
+                            wrapperSize={42}
+                            size={17}
+                            color={'rgb(228, 241, 254)'}
+                            activeColor={'#f28'}
+                        />
+                    </SnsIconWrapper>
+                    <SnsIconWrapper>
+                        <Icon
+                            name="facebook"
+                            wrapperSize={42}
+                            size={17}
+                            color={'rgb(228, 241, 254)'}
+                            activeColor={'#f28'}
+                        />
+                    </SnsIconWrapper>
+                    <SnsIconWrapper>
+                        <Icon
+                            name="instagram"
+                            wrapperSize={42}
+                            size={17}
+                            color={'rgb(228, 241, 254)'}
+                            activeColor={'#f28'}
+                        />
+                    </SnsIconWrapper>
+                    <SnsIconWrapper>
+                        <Icon
+                            name="skype"
+                            wrapperSize={42}
+                            size={17}
+                            color={'rgb(228, 241, 254)'}
+                            activeColor={'#f28'}
+                        />
+                    </SnsIconWrapper>
                 </HeaderTopLeft>
                 <HeaderTopRight>
                     <LocationNumberWrapper>
-                        <LocationNumberIcon src={phoneIcon} />
+                        <Icon
+                            name="phone"
+                            wrapperSize={32}
+                            size={20}
+                            color={'rgb(228, 241, 254)'}
+                        />
                         <span> 1-9870654-3210</span>
                     </LocationNumberWrapper>
                     <LocationNumberWrapper>
-                        <LocationNumberIcon src={locationIcon} />
+                        <Icon
+                            name="location"
+                            wrapperSize={32}
+                            size={20}
+                            color={'rgb(228, 241, 254)'}
+                        />
                         <span> 123, Regal Garden, London, UK</span>
                     </LocationNumberWrapper>
                 </HeaderTopRight>
@@ -62,34 +100,18 @@ const HeaderTopLeft = styled.div`
     align-items: center;
 `;
 
-const IconWrapper = styled.div<{ hoverColor?: string }>`
-    width: 42px;
-    padding: 12px;
-    box-sizing: border-box;
-    border-right: 1px solid rgba(255, 255, 255, 0.15);
-    cursor: pointer;
-
-    &:hover {
-        background-color: ${(props) => props.hoverColor};
-    }
-`;
-
-const IconStyled = styled.img`
-    width: 17px;
-`;
-
 const HeaderTopRight = styled.div`
     display: flex;
-    align-items: center;
-    gap: 24px;
+    gap: 20px;
 `;
+
 const LocationNumberWrapper = styled.div`
     display: flex;
     align-items: center;
-    gap: 6px;
-    color: rgb(222, 222, 222);
+    color: rgb(228, 241, 254);
 `;
 
-const LocationNumberIcon = styled.img`
-    width: 20px;
+const SnsIconWrapper = styled.div`
+    display: flex;
+    border-right: 1px solid rgb(228, 241, 254, 0.15);
 `;
